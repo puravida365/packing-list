1b20c87f4236c39e8e13f38163a6c1be8f73d02a
Josue Mora <josue@inno-live.com>
2018-07-11 09:30:50 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+// Welcome componenet
+const Welcome = (props) => {
+  return (
+    <Text style={styles.welcome}>
+      {props.name || "Hey"}, GOODBYE to Portland, OR!
+    </Text>
+  )
+}
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Welcome name="AJ" />
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
